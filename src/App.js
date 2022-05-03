9eeb8f400512e6431488a9d35598b6c3bce7b4ff
SaraAzmin <azmin.sara17@gmail.com>
2022-05-03 22:07:21 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from '../Shared/Header/Header';
+import Footer from '../Shared/Footer/Footer'
 
 const Home = () => {
     return (
+
         <div>
+            <Header></Header>
             <h1 className='text-7xl'>this is home</h1>
+            <Footer></Footer>
         </div>
     );
 };
