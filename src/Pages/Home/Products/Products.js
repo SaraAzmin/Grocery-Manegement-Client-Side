b4da6d1e31442311876c02ee29b6bada4dec1ec2
SaraAzmin <azmin.sara17@gmail.com>
2022-05-04 19:44:50 +0200

    product card display added

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 
 const Products = () => {
 
@@ -11,9 +12,16 @@ const Products = () => {
     }, []);
 
     return (
-        <div>
-            <h1>this is for products: {products.length}</h1>
-
+        <div id='services'>
+            <div className='mx-10 md:mx-20 mb-10' >
+                <h2 className='text-2xl md:text-4xl pt-5 md:pt-12 font-semibold text-green-600 drop-shadow-2xl uppercase'>Products Available</h2>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-5 md:py-14'>
+                    {
+                        products.map(product => <Product key={product._id} product={product}></Product>)
+                    }
+                </div>
+                <button type="button" class=" inline-block px-6 py-4 bg-green-600 text-white font-semibold text-lg leading-tight rounded shadow-md hover:bg-green-800 hover:shadow-lg">Manage Inventory</button>
+            </div>
         </div>
     );
 };
